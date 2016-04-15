a4be91316a07c36b56729b23bfe972b6dd9d95c6
Manev100 <marc.neveling@uni-duesseldorf.de>
2016-04-15 15:32:49 +0200

    Camera

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate(){
+		transform.LookAt(player.transform);
+	}
+}
